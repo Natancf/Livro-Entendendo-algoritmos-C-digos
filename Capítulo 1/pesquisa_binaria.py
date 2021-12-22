ca6ba262e49e897260e02da4132a0219ce69f12e
Natancf <202017728@aluno.unb.br>
2021-12-21 22:38:16 -0300

    Comentários

--- Capítulo 1/pesquisa_binaria.py
+++ Capítulo 1/pesquisa_binaria.py
@@ -2,14 +2,14 @@ def pesquisa_binaria(lista, item):
     baixo = 0 
     alto = len(lista) - 1
 
-    while baixo <= alto: #O laço continua enquanto não conseguir chegar a um unico elemento
-        meio = (baixo + alto) // 2
-        chute = lista[meio]
-        if chute == item:
+    while baixo <= alto: #O laço continua enquanto não conseguir chegar a um único elemento
+        meio = (baixo + alto) // 2 #Verifica o elemeto central
+        chute = lista[meio] 
+        if chute == item: #Verifica se o elemento central é igual o item
             return meio
-        if chute > item:
+        if chute > item: #Verifica seo chute foi muito alto
             alto = meio -1
-        else:
+        else: #Verifica se o chute foi muito baixo 
             baixo = meio + 1
     return None
 
