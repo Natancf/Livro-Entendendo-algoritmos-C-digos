b432777a1df666649a2fe1fbd9c5bd8ab4b194de
Natancf <202017728@aluno.unb.br>
2021-12-22 22:22:25 -0300

    ordenacao por selecao

--- Capítulo 2/ordenacao_por_selecao.py
+++ Capítulo 2/ordenacao_por_selecao.py
@@ -1,8 +1,8 @@
 def buscaMenor(arr):
-    menor = arr[0]
-    menor_indice = 0
+    menor = arr[0] #guarda o menor valor
+    menor_indice = 0 #guarda o indice do menor valor
     for i in range(1, len(arr)):
-        if arr[i] < menor:
+        if arr[i] < menor: #verifica qual é o menor valor é o coloca na variavel menor
             menor = arr[i]
             menor_indice = i
     return menor_indice
@@ -10,7 +10,7 @@ def buscaMenor(arr):
 def ordenacaoporselecao(arr):
     novoArr = []
     for i in range(len(arr)):
-        menor = buscaMenor(arr)
+        menor = buscaMenor(arr) #encontra qual é o menor elemento do array e o adiciona no novo array
         novoArr.append(arr.pop(menor))
     return novoArr
 
